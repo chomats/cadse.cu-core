10deede7be9968eece3f77e7bae14275a598913e
Stephane Chomat <Stephane.chomat@imag.fr>
2010-01-29 06:43:11 +0100

    fix page and group

--- src/main/java/fr/imag/adele/cadse/core/impl/attribute/LongAttributeType.java
+++ src/main/java/fr/imag/adele/cadse/core/impl/attribute/LongAttributeType.java
@@ -25,7 +25,9 @@ import fr.imag.adele.cadse.core.Item;
 import fr.imag.adele.cadse.core.ItemType;
 import fr.imag.adele.cadse.core.attribute.CheckStatus;
 import fr.imag.adele.cadse.core.attribute.IAttributeType;
+import fr.imag.adele.cadse.core.impl.ui.UIFieldImpl;
 import fr.imag.adele.cadse.core.transaction.delta.ItemDelta;
+import fr.imag.adele.cadse.core.ui.EPosLabel;
 import fr.imag.adele.cadse.core.ui.UIField;
 import fr.imag.adele.cadse.core.ui.UIPlatform;
 import fr.imag.adele.cadse.core.util.Convert;
@@ -146,7 +148,6 @@ public class LongAttributeType extends AttributeType implements
 
 	@Override
 	public UIField generateDefaultField() {
-		// TODO Auto-generated method stub
-		return null;
+		return new UIFieldImpl(CadseGCST.DTEXT, UUID.randomUUID(), this, getDisplayName(), EPosLabel.left, null, null);
 	}
 }
