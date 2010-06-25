d85cb3d13fb03c6048af6a1214903a5e8c27892f
Stephane Chomat <Stephane.Chomat@imag.fr>
2010-06-25 11:44:12 +0200

    implements export bundle

---
+++ src/main/java/fr/imag/adele/cadse/core/impl/BundleInstallActivator.java
@@ -0,0 +1,44 @@
+package fr.imag.adele.cadse.core.impl;
+
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import fr.imag.adele.cadse.core.Item;
+import fr.imag.adele.cadse.core.transaction.LogicalWorkspaceTransaction;
+import fr.imag.adele.cadse.core.transaction.delta.ItemDelta;
+
+public class BundleInstallActivator implements BundleActivator {
+
+	//public static final String CADSE_ZIP = "cadse.zip";
+	Set<UUID> itemsloaded = new HashSet<UUID>();
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		URL cadsezip = context.getBundle().getEntry("/");
+		
+		final ExportImportCadseFunction exportImportCadseFunction = new ExportImportCadseFunction();
+		exportImportCadseFunction.importCadseItems(cadsezip);
+		
+		for (Item item : exportImportCadseFunction.getItemsHash()) {
+			itemsloaded.add(item.getId());
+		};
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		//delete items;
+		LogicalWorkspaceTransaction t = CadseCore.getLogicalWorkspace().createTransaction();
+		for (UUID key : itemsloaded) {
+			ItemDelta item = t.getItem(key);
+			if (item == null || item.isDeleted()) continue;
+			item.unload();
+		}
+		t.commit();
+	}
+
+}
