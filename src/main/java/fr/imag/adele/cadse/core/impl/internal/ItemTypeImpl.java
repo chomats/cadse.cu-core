6cf0c38f334e5d3d183bee5b3dd7fb341fe61759
Stephane Chomat <Stephane.chomat@imag.fr>
2010-07-28 11:49:28 +0200

    fix bug diana

--- src/main/java/fr/imag/adele/cadse/core/impl/internal/ItemTypeImpl.java
+++ src/main/java/fr/imag/adele/cadse/core/impl/internal/ItemTypeImpl.java
@@ -1447,7 +1447,7 @@ public class ItemTypeImpl extends TypeDefinitionImpl implements ItemType,
 			return (IAttributeType<?>[]) ret.toArray(new IAttributeType<?>[ret
 					.size()]);
 		}
-		return getType().getAllAttributeTypes();
+		return super.getLocalAllAttributeTypes();
 	}
 	
 	
