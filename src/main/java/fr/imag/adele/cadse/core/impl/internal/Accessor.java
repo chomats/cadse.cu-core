5acce9b34663aab51453152cd44fbb311188d486
Stephane Chomat <Stephane.chomat@gmail.com>
2010-09-07 22:20:13 +0200

    add license

--- src/main/java/fr/imag/adele/cadse/core/impl/internal/Accessor.java
+++ src/main/java/fr/imag/adele/cadse/core/impl/internal/Accessor.java
@@ -2,10 +2,10 @@
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding transactionright ownership.  The ASF licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a transaction of the License at
+ * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -15,6 +15,8 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
+ * Copyright (C) 2006-2010 Adele Team/LIG/Grenoble University, France
  */
 package fr.imag.adele.cadse.core.impl.internal;
 
